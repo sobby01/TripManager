a4ab81d9bd683866ae07af1f69107dce10fb6fb0
Saurabh Singh <saurabh.singh@winshuttle.com>
2020-01-13 11:34:33 +0530

    breadcrumb implemented

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { LoginComponent } from './login/login.component'
 import { Router, ActivatedRoute } from '@angular/router';
@@ -8,8 +8,10 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  private modalRef: BsModalRef
+export class AppComponent implements OnInit , OnDestroy{
+  private modalRef: BsModalRef;
+  breadcrumbList: Array<any> = [];
+  myObserver = null;
   constructor(
     private modalService: BsModalService,
     private router: Router,
@@ -17,7 +19,12 @@ export class AppComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.listenRouting();
   }
+
+  ngOnDestroy() {
+    this.myObserver.unsubscribe();
+}
 
   Login(){
     const initialState = {
@@ -30,5 +37,45 @@ export class AppComponent implements OnInit {
       { class: 'mrk-def-modal approve-task-modal modal-dialog-centered' }, 
       { initialState })
     );
+  }
+
+  listenRouting() {
+    let routerUrl: string, routerList: Array<any>, target: any;
+    this.myObserver = this.router.events.subscribe((router: any) => {
+      routerUrl = router.urlAfterRedirects;
+
+      if (routerUrl && typeof routerUrl === 'string') {
+        routerList = routerUrl.slice(1).split('/');
+        if(routerList.length > 0){
+          this.breadcrumbList.length = 0;
+        routerList.forEach((router, index) => {
+          
+          if(router != ''){
+           
+            this.breadcrumbList.push({
+              name: router,
+              path: (index === 0) ? router :  `${this.breadcrumbList[index-1].path}`
+            });
+          }
+        
+         
+        });}
+        else{
+          if(this.breadcrumbList.length<=0 ||this.breadcrumbList.find(x => x.Name=='Home') < 0){
+          this.breadcrumbList.push({
+            name: 'Home',
+            path: 'Home'
+          });}
+        }
+      }
+      else if(routerUrl == undefined){
+        if(this.breadcrumbList.length<=0 || this.breadcrumbList.find(x => x.Name=='Home') < 0){
+          this.breadcrumbList.push({
+            name: 'Home',
+            path: 'Home'
+          });
+        }       
+      }
+    });
   }
 }
